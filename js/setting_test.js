0defc622e8caa8644d35f19f2fcc0d7c4497ce8c
Takashi Beppu <bepsu@stf.jan.ne.jp>
2013-12-12 13:12:53 +0900

    setting.js

---
+++ js/setting_test.js
@@ -0,0 +1,24 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'nagai_budget_2013';
+OpenSpending.year = '2013';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* yokohama cofog */
+  '20130110': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' }
+};
+
+
