8d5b00e9f98826478d70a0bc3f1066fd6c6706d4
Takashi Beppu <bepsu@stf.jan.ne.jp>
2014-01-27 11:21:17 +0900

    a

--- js/dailybread.js
+++ js/dailybread.js
@@ -208,7 +208,7 @@ OpenSpending.DailyBread = function (elem) {
           paths = svg.getElementsByTagName('path');
           for (j=0;j<paths.length;j++) joined += paths[j].getAttribute('d')+' ';
           icon = paper.path(joined);
-          icon.attr({ fill: 'white', stroke: 'none' });
+//          icon.attr({ fill: 'white', stroke: 'none' });
           icon.scale(iconRad/50, iconRad/50, 0, 0);
         } catch (e) {
           // failed to handle SVG
@@ -246,7 +246,7 @@ OpenSpending.renderDependentTypes = function(db) {
       paths = svg.getElementsByTagName('path');
       for (j=0;j<paths.length;j++) joined += paths[j].getAttribute('d')+' ';
       icon = r.path(joined);
-      icon.attr({ fill: 'white', stroke: 'none' });
+//      icon.attr({ fill: 'white', stroke: 'none' });
       icon.scale(iconRad/50, iconRad/50, 0, 0);
     });
   }
