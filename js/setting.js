a06aa921c7f237e622993241235f1b65bf885644
Takashi Beppu <bepsu@stf.jan.ne.jp>
2014-04-14 14:17:02 +0900

    aa

--- js/setting.js
+++ js/setting.js
@@ -14,10 +14,6 @@ var UseData = UseData || {};
 //OpenSpending.identifier = 'nagai_budget_2014';
 //OpenSpending.year = '2014';
 
-//UseData.identifier = 'nagai_budget_2014';
-//UseData.year = '2014';
-
-
 		var url = location.href;
 		var params = url.split("?");
 		var paramms = [];
